8f456cde33d76f79038a8028c1d3326edf04e0b7
jayjoshi64 <jayjoshi7064@gmail.com>
2017-12-27 15:17:26 +0530

    the game begins

---
+++ server.py
@@ -0,0 +1,41 @@
+
+class game:
+
+
+
+    #end game
+
+
+#hashmap here
+
+#end hashmap
+
+
+def sendMsg(msg,fromclient,toclient):
+
+
+    
+    #end sendmsg
+
+
+
+def cpu(client,msg):
+
+
+
+
+    #end cup
+
+
+def handleClient():
+
+
+
+    #end handleClient 
+
+
+while True:
+
+
+
+    #end reciever
